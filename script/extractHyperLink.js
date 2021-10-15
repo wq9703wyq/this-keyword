0a55389867cdc49531d4a880b3c739cec575ce63
鹿角兔子 <w464326042@gmail.com>
2021-10-15 20:04:42 +0800

    add hyperlink

--- script/extractHyperLink.js
+++ script/extractHyperLink.js
@@ -4,7 +4,7 @@
  * @Author: 鹿角兔子
  * @Date: 2021-10-13 13:33:01
  * @LastEditors: 鹿角兔子
- * @LastEditTime: 2021-10-13 23:55:45
+ * @LastEditTime: 2021-10-14 02:11:26
  */
 const fs = require("fs");
 const { Original_Doc, Link_Json } = process.env;
@@ -14,7 +14,7 @@ fs.stat(Link_Json, (err, stats) => {
         fs.writeFileSync(Link_Json, "");
     }
     // 读取超链接文件
-    const hyperLink = JSON.parse(fs.readFileSync(Link_Json, {encoding: "utf8"})) || {};
+    const hyperLink = JSON.parse(fs.readFileSync(Link_Json, {encoding: "utf8"}) || "{}");
     // 读取文档
     const doc = fs.readFileSync(Original_Doc, { encoding: "utf8" });
     const linkRegex = /\[+[\u4e00-\u9fa5_a-zA-Z0-9- ]+\]+(?=\(\))/g
